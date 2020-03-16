393da6bd5c0245d428ed0cd034b8dd636641b87b
Arjun Balasubramanian <arjunbala0711@gmail.com>
2020-03-16 12:20:29 -0500

    Hello world repo structure and maven compilation

---
+++ README.md
@@ -0,0 +1,5 @@
+## To compile
+mvn compile
+
+## To run code
+mvn exec:java -Dexec.mainClass="com.ngdb.htapscheduling.Simulation"
---
+++ src/main/java/com/ngdb/htapscheduling/Simulation.java
@@ -0,0 +1,7 @@
+package com.ngdb.htapscheduling;
+
+public class Simulation {
+	public static void main(String args[]) {
+		System.out.println("Hello world");
+	}
+}
