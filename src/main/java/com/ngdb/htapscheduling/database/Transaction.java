037866b210262748e803da9a989cdd33837d77ca
kratiagrawal <krati1610@gmail.com>
2020-04-04 20:34:59 -0500

    debugged error

--- src/main/java/com/ngdb/htapscheduling/database/Transaction.java
+++ src/main/java/com/ngdb/htapscheduling/database/Transaction.java
@@ -69,9 +69,6 @@ public class Transaction {
 		return mOutputSize;
 	}
 
-	public void setOutputSize(Double output_size) {
-		mOutputSize = output_size;
-	}
 	/**
 	 * Add an item to the read set
 	 * 
