0a0f4679a4b32126ee83b5737ba2096a5e08c4fd
Giri Prasanna M <Giri Prasanna M@192.168.1.21>
2020-04-06 01:14:10 -0500

    Added equations to getHostToDeviceTransferTime and getDeviceToHostTransferTime

--- src/main/java/com/ngdb/htapscheduling/cluster/PCIeUtils.java
+++ src/main/java/com/ngdb/htapscheduling/cluster/PCIeUtils.java
@@ -2,12 +2,16 @@ package com.ngdb.htapscheduling.cluster;
 
 public class PCIeUtils {
 	public static Double getHostToDeviceTransferTime(Double sizeKB) {
-		// TODO: Implement
-		return 0.0;
+		if (sizeKB < 16384.0)
+			return 1.0;
+		else
+			return Math.ceil(0.0001205*sizeKB-0.3629);
 	}
 	
 	public static Double getDeviceToHostTransferTime(Double sizeKB) {
-		// TODO:
-		return 0.0;
+		if (sizeKB <= 2048)
+			return 1.0;
+		else
+			return Math.ceil(0.0003490*sizeKB+0.1357);
 	}
 }
